ca26950e46616b4099745f922e7ddde84ea1f2e0
487651@mail.ru <487651@mail.ru>
2023-11-30 13:50:32 +0400

    30112023

--- NewStudyRostelecom/news/models.py
+++ NewStudyRostelecom/news/models.py
@@ -1,8 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Tag(models.Model):
+    title = models.CharField(max_length=80)
+    status=models.BooleanField(default=True)
 
-# Create your models here.
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['title', 'status']
+        verbose_name = 'Тэг'
+        verbose_name_plural = 'Тэги'
+
 class Article(models.Model):
     categories = (('E', 'Economics'),
                   ('S', 'Science'),
@@ -13,6 +23,7 @@ class Article(models.Model):
     text = models.TextField('Текст новости')
     date = models.DateTimeField('Дата публикации', auto_created=True)
     category = models.CharField(choices=categories, max_length=20, verbose_name='Категории')
+    tag=models.ManyToManyField(to=Tag)
 
     def __str__(self):
         return f'Новость: {self.title} от {self.date}'
@@ -24,3 +35,5 @@ class Article(models.Model):
         ordering=['title','date']
         verbose_name = 'Новость'
         verbose_name_plural = 'Новости'
+
+
