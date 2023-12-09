db268042b0823972f9e15cac4fc47f53af240b04
Pavel <487651@mail.ru>
2023-12-09 22:25:07 +0400

    09122023

--- NewStudyRostelecom/news/views.py
+++ NewStudyRostelecom/news/views.py
@@ -7,20 +7,6 @@ from .forms import *
 
 # Create your views here.
 
-# def news_list(request):
-#     # news = Article.objects.filter(author=request.user.id)
-#     # news = Article.objects.get(title='Новость 1')
-#     # print(news)
-#     # news = Article.objects.filter(author=request.user.id)
-#     news = Article.objects.all()
-#     tag = Tag.objects.filter(title='Crypto')[0]
-#     tagged_news = Article.objects.filter(tags=tag)
-#     print(tagged_news)
-#     # tag=Tag.objects.filter(title='IT').first()
-#
-#     context = {'news': news}
-#     return render(request, 'news/news_list.html', context)
-
 def news_list(request):
     categories = Article.categories  # создали перечень категорий
     author_list = User.objects.all()  # создали перечень авторов
