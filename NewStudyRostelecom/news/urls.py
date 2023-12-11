26be292b0171c1f2013fd0d3b1b8bfd1f486b211
Pavel <487651@mail.ru>
2023-12-11 23:46:56 +0400

    09122023

--- NewStudyRostelecom/news/urls.py
+++ NewStudyRostelecom/news/urls.py
@@ -1,4 +1,3 @@
-
 from django.contrib import admin
 from django.urls import path
 from . import views
@@ -8,4 +7,6 @@ urlpatterns = [
     path('', views.news_list, name='news_list'),
     path('load', views.news_load, name='news_load'),
     path('create_article', views.create_article, name='create_article'),
+    path('news_search', views.news_search, name='news_search'),
+    path('search_auto/', views.search_auto, name='search_auto'),
 ]
--- NewStudyRostelecom/news/views.py
+++ NewStudyRostelecom/news/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render, HttpResponse, redirect
 from .models import *
 import pandas as pd
 import random
+import json
 from .forms import *
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
+
 
 # Create your views here.
 
@@ -33,9 +35,10 @@ def news_list(request):
 
 def news_detail(request, id):
     news = Article.objects.get(id=id)
-    images=Image.objects.filter(article_id=id)
-    context = {'news': news,'images': images}
+    images = Image.objects.filter(article_id=id)
+    context = {'news': news, 'images': images}
     return render(request, 'news/news_detail.html', context)
+
 
 def news_load(request):
     cntr = Article.objects.count() + 1
@@ -55,6 +58,7 @@ def news_load(request):
         cntr += 1
     return HttpResponse('Новости добавлены')
 
+
 @login_required(login_url=settings.LOGIN_URL)
 def create_article(request):
     if request.method == 'POST':
@@ -64,7 +68,7 @@ def create_article(request):
             if current_user.id != None:
                 new_article = Article()
                 new_article.author = current_user
-                new_article.title=form.cleaned_data.get('title')
+                new_article.title = form.cleaned_data.get('title')
                 new_article.anouncement = form.cleaned_data.get('anouncement')
                 new_article.text = form.cleaned_data.get('text')
                 new_article.save()
@@ -76,3 +80,26 @@ def create_article(request):
         form = ArticleForm()
     context = {'form': form}
     return render(request, 'news/create_article.html', context)
+
+
+def news_search(request):
+    news={}
+    if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+        q = request.GET.get('term','')
+        news = Article.objects.filter(title__icontains=q)
+    context = {'news': news}
+    return render(request, 'news/news_search.html', context)
+
+def search_auto(request):
+    news={}
+    if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+        q = request.GET.get('term','')
+        news = Article.objects.filter(title__icontains=q)
+        results=[]
+        for n in news:
+            results.append(n.title)
+        data=json.dumps(results)
+    else:
+        data='fail'
+    mimetype='application/json'
+    return HttpResponse()
