b47ddd662c5dad99c2260654290117e000188e21
Pavel <487651@mail.ru>
2023-12-25 22:03:53 +0400

    25122023(2)

--- NewStudyRostelecom/NewStudyRostelecom/settings.py
+++ NewStudyRostelecom/NewStudyRostelecom/settings.py
@@ -127,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'main/static')
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
