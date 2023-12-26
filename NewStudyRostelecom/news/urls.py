f761ac5cbffd0e32899d9fa9c5574da2081a4ea5
Pavel <487651@mail.ru>
2023-12-26 12:10:13 +0400

    26122023

--- NewStudyRostelecom/news/urls.py
+++ NewStudyRostelecom/news/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('<int:id>', views.news_detail, name='news_detail'),
     path('', views.news_list, name='news_list'),
-    path('load', views.news_load, name='news_load'),
+    # path('load', views.news_load, name='news_load'),
     path('create_article', views.create_article, name='create_article'),
     path('news_search', views.news_search, name='news_search'),
     path('search_auto/', views.search_auto, name='search_auto'),
--- NewStudyRostelecom/news/views.py
+++ NewStudyRostelecom/news/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, HttpResponse, redirect
 from .models import *
-import pandas as pd
 import random
 import json
 from .forms import *
@@ -64,23 +63,23 @@ def news_detail(request, id):
     return render(request, 'news/news_detail.html', context)
 
 
-def news_load(request):
-    cntr = Article.objects.count() + 1
-    df = pd.read_excel(r'C:/Users/Pavel/Downloads/news_gpt.xlsx', dtype='string')
-    id_list = User.objects.all().values('id')
-    lst = []
-    for lst_ in id_list:
-        lst.append(lst_.get('id'))
-    for r in range(0, df.shape[0]):
-        id_ = random.randint(0, len(lst) - 1)
-        author = User.objects.get(id=lst[id_])
-        title = 'Новость ' + str(cntr)
-        anouncement = df.loc[r, 'anounce']
-        text = df.loc[r, 'text']
-        new_article = Article(author=author, title=title, anouncement=anouncement, text=text)
-        new_article.save()
-        cntr += 1
-    return HttpResponse('Новости добавлены')
+# def news_load(request):
+#     cntr = Article.objects.count() + 1
+#     df = pd.read_excel(r'C:/Users/Pavel/Downloads/news_gpt.xlsx', dtype='string')
+#     id_list = User.objects.all().values('id')
+#     lst = []
+#     for lst_ in id_list:
+#         lst.append(lst_.get('id'))
+#     for r in range(0, df.shape[0]):
+#         id_ = random.randint(0, len(lst) - 1)
+#         author = User.objects.get(id=lst[id_])
+#         title = 'Новость ' + str(cntr)
+#         anouncement = df.loc[r, 'anounce']
+#         text = df.loc[r, 'text']
+#         new_article = Article(author=author, title=title, anouncement=anouncement, text=text)
+#         new_article.save()
+#         cntr += 1
+#     return HttpResponse('Новости добавлены')
 
 
 @check_group('Authors')
